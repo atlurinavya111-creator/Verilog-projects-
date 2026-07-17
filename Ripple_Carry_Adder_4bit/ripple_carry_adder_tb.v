112ad0ef0808c8fb488286ce68ebf9d89c624a85
atlurinavya111-creator <atlurinavya111@gmail.com>
2026-07-18 02:41:48 +0530

    Add testbench for 4-bit ripple carry adder

---
+++ Ripple_Carry_Adder_4bit/ripple_carry_adder_tb.v
@@ -0,0 +1,25 @@
+module ripple_carry_adder_tb;
+
+reg [3:0] a, b;
+reg cin;
+wire [3:0] sum;
+wire cout;
+
+ripple_carry_adder uut (
+    .a(a),
+    .b(b),
+    .cin(cin),
+    .sum(sum),
+    .cout(cout)
+);
+
+initial begin
+    a = 4'b0000; b = 4'b0000; cin = 0; #10;
+    a = 4'b0011; b = 4'b0101; cin = 0; #10;
+    a = 4'b1111; b = 4'b0001; cin = 0; #10;
+    a = 4'b1010; b = 4'b0101; cin = 1; #10;
+
+    $finish;
+end
+
+endmodule
